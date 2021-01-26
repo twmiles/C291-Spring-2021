7ac6aa458dcc39e879d8d07ba909cf0de2adea24
Tracy William Robert Miles <twmiles@silo.soic.indiana.edu>
2021-01-25 20:36:24 -0500

    modified hello.c to print my name and age

--- exercises/wk1/hello.c
+++ exercises/wk1/hello.c
@@ -1,16 +1,14 @@
 /* 
- * Basic C program to print out hello world! 
- * Author: Adeel Bhutta
+ * Basic C program to print out my name and age 
+ * Author: Tracy Miles
 */
 
 #include <stdio.h>
 
 int main(void) {
-  int myNumber = 0;
-  printf("Hello: Welcome to C291!\n");
-  printf("Please enter a number and press ENTER: ");
-  scanf("%d", &myNumber);  
-  printf("Here is the number yor entered: %d\n",myNumber);
+  int myNumber = 21;
+  printf("Miles, Tracy ");
+  printf("%04d", myNumber);
   return(0); 
 }
 
