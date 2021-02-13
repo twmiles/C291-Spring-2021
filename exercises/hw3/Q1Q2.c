938943affff271d73b9d10dc8a0e224c6d956511
Tracy William Robert Miles <twmiles@silo.soic.indiana.edu>
2021-02-12 23:53:06 -0500

    assignment3

--- exercises/hw3/Q1Q2.c
+++ exercises/hw3/Q1Q2.c
@@ -8,59 +8,59 @@
 #include<stdlib.h>
 
 int main(void){
-  //a 
+  //a  //this is for left justified 
  int a = 100100;
   printf("left justified output:\n%-12u", a);
-  //b
+  //b //Print 7.350 in a 8-digit field with preceding zeros.
   double b = 7.350;
   printf("\n%08.3f\n", b);
-  //c
+  //c //Print 300 with and without a sign.
   int c = 300;
   printf("signed: %+d unsigned: %u\n", c,c);
-  //d
+  //d //Print -300 as unsigned number.
   printf("%u\n", -300);
-  //e
+  //e //Read a hexadecimal value into variable hex.
   int e;
   printf("enter hex number: ");
   scanf("%x", &e);
   printf("hex value: %x, decimal number: %d\n", e,e);
-  //f
+  //f //Read an octal value into variable octal.
   int oct;
   printf("enter an octal value: ");
   scanf("%o", &oct);
   printf("octal value:%o\n", oct); 
-  //g
+  //g //Print 100 in hexadecimal form preceded by 0x.
   int g = 100;
   printf("hex: 0x%x\n", g);
-  //h
+  //h //Print 100 in octal form preceded by 0.
   int h = 100;
   printf("100 in octal form: 0%o\n", h);
-  //i
+  //i //Read characters into array m until the letter z is encountered.
   char str1[100];                                                                                                 
   printf("enter characters: ");                                                                                     
   scanf("%[^z]s", &str1); 
   printf("Entered string is: %s\n", str1);
-   
-  //j                                                                                                            
-  int hr, min, second;                                                               
-  printf("enter a time (hh-mm-ss): ");                                                                        
+
+  //j //Read a time of the form hh-mm-ss, storing the parts of the time in the integer variables hour, minute and second. Skip the dash (-) in the input stream. Use the assignment suppression character.
+  int hr, min, second;
+  printf("enter a time (hh-mm-ss): ");
   scanf("%d%*c%d%*c%d", &hr, &min, &second);
   printf("hour: %d minute: %d second: %d\n", hr, min, second);
   
-  //k
+  //k //Read a time of the form hh:mm:ss, storing the parts of the time in the integer variables hour, minute and second. Skip the colons (:) in the input stream. Do not use the assignment suppression character.
   int hr1, min1, second1;
   printf("enter a time (hh:mm:ss): ");
   scanf("%d:%d:%d", &hr1, &min1, &second1);
   printf("hour: %d minute: %d second: %d\n", hr1, min1, second1);
-  //l
-
+  
+  //l //Read a string of the form "characters" from the standard input. Store the string in character array s. Eliminate the quotation marks from the input stream.
   char str[50];
   scanf("%s", str);
-  char *ptr = str;
-  ptr++;
-  ptr[strlen(ptr)-1] = '\0';
-  for(;*ptr!='\0';ptr++)
-    printf("%c",*ptr);
+  char *x = str;
+  x++;
+  x[strlen(x)-1] = '\0';
+  for(;*x!='\0';x++)
+    printf("%c",*x);
 
   printf("\nQUESTION 2\n");
 
