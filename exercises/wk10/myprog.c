dd37af26182af402ca94c2f58206c227154a01b8
Tracy William Robert Miles <twmiles@silo.soic.indiana.edu>
2021-04-09 21:37:59 -0400

    assignment9

--- exercises/wk10/myprog.c
+++ exercises/wk10/myprog.c
@@ -7,21 +7,21 @@ char alphabet[MAX_LETTERS];
 void initialize_alphabet(char *a) {
   char *p;
   char current_letter = 'a';
-  for (p=a;p<(a+MAX_LETTERS);p++);{
+  for (p=a;p<(a+MAX_LETTERS);p++) { //remove ;
     *p = current_letter++;
   }
 }  
 
 void reverse_print_alpha(char *b) {
-  static var = 0;
+  static var = 1; //make equal to 1
   char *p;
   
   var?(p=NULL):(b=NULL);
   var *= 1;
   printf("\nReverse alpha...\n");
   initialize_alphabet(b);
-  for (p=(b+MAX_LETTERS-1);p>=b;p--);{
-    printf("%c ",*b);
+  for (p=(b+MAX_LETTERS-1);p>=b;p--){ //remove ;
+    printf("%c ",*p);
   }
   printf("\n");
   return ;
